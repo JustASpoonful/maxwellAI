20e072d5f8a758a8ef50ed2bfe596e3c9d606176
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2024-10-10 20:30:09 -0400

    Update and rename index.js to responses.js

---
+++ responses.js
@@ -0,0 +1,33 @@
+const responses = {
+    "hello": [
+        "Hi there!",
+        "Hello!",
+        "Greetings!",
+        "Hey!"
+    ],
+    "how are you?": [
+        "I'm just a bunch of code, but I'm doing well!",
+        "Feeling great, thanks for asking!",
+        "I'm here to help you!",
+        "Just processing data!"
+    ],
+    "what is your name?": [
+        "I'm a chatbot!",
+        "You can call me Chatbot!",
+        "I'm just your friendly assistant!",
+        "I'm nameless, but I'm here for you!"
+    ],
+    "what's the weather like?": [
+        "I can't check the weather, but I hope it's nice!",
+        "I don't have access to weather data.",
+        "You might want to check a weather website!",
+        "Weather? I wish I could feel it!"
+    ],
+    "math": [
+        "I can do some math! What do you want to calculate?",
+        "Sure, ask me a math question!",
+        "I can help with math, just let me know!",
+        "What kind of math problem do you have?"
+    ],
+    // Add more predefined responses here as needed
+};
