83d9d634ec7742ec542e3f9595325a836e24fe3d
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2024-10-12 00:21:48 -0400

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,7 +1,7 @@
 const predefinedResponses = [
     {
         triggers: ["hi", "hello", "hey", "yo", "howdy", "greetings"],
-        replies: ["Hello! How can I help you today?", "Hi there!", "Hey! How's it going?" "Is there anything you need help with?"]
+        replies: ["Hello! How can I help you today?", "Hi there!", "Hey! How's it going?", "Is there anything you need help with?"]
     },
     {
         triggers: ["bye", "goodbye"],
@@ -15,8 +15,8 @@ const predefinedResponses = [
         triggers: ["tell me something fun", "say something funny", "make me laugh"],
         replies: ["Why don't scientists trust atoms? Because they make up everything!", "I would tell you a joke about pizza, but it's too cheesy!", "I’m reading a book on anti-gravity. It’s impossible to put down!"]
     },
-     {
+    {
         triggers: ["your purpose"],
-        replies: ["I am a going to be a utility that can help with math and do other extremely cool stuff :) This is the only predefined answer to your question so if you ask multiple times, it will just end up being this."]
+        replies: ["I am a utility that can help with math and do other extremely cool stuff :) This is the only predefined answer to your question so if you ask multiple times, it will just end up being this."]
     }
 ];
